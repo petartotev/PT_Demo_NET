410d739d592ff49342588dd3aa312cd88c819147
petartotev <petar@petartotev.net>
2023-10-20 19:22:23 +0300

    .NET8 demos

---
+++ src/NET8/DataValidationObject.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NET8
+{
+    // ==================== Data validation ====================
+    // https://learn.microsoft.com/en-us/dotnet/core/whats-new/dotnet-8#data-validation
+    public class DataValidationObject
+    {
+        // RangeAttribute.MinimumIsExclusive & RangeAttribute.MaximumIsExclusive
+        // Specifies whether bounds are included in the allowable range.
+        [Range(10, 20, MinimumIsExclusive = false, MaximumIsExclusive = false)]
+        public int RangedInt { get; set; }
+
+        // System.ComponentModel.DataAnnotations.LengthAttribute
+        // Specifies both lower and upper bounds for strings or collections.
+        // For example, [Length(10, 20)] requires at least 10 elements and at most 20 elements in a collection.
+        [Length(10, 20)]
+        public string MinMaxLengthString { get; set; }
+
+        // System.ComponentModel.DataAnnotations.Base64StringAttribute
+        // Validates that a string is a valid Base64 representation.
+        [Base64String]
+        public string ValidBase64 { get; set; }
+
+        // System.ComponentModel.DataAnnotations.AllowedValuesAttribute
+        // Specify allow lists and deny lists, respectively. For example, [AllowedValues("apple", "banana", "mango")].
+        [AllowedValues("apple", "banana", "mango")]
+        public string AllowedValuesString { get; set; }
+
+        // System.ComponentModel.DataAnnotations.DeniedValuesAttribute
+        // Specify allow lists and deny lists, respectively. For example, [AllowedValues("apple", "banana", "mango")]. 
+        [DeniedValues("apple", "banana", "mango")]
+        public string DeniedValuesString { get; set; }
+    }
+}
---
+++ src/NET8/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace NET8;
+
+internal class Program
+{
+    static void Main()
+    {
+        // ==================== Methods for working with randomness ====================
+        // https://learn.microsoft.com/en-us/dotnet/core/whats-new/dotnet-8#methods-for-working-with-randomness
+
+        // --------------- GetItems<T>() ---------------
+
+        var colors = new[] { "red", "green", "blue", "cyan", "magenta", "yellow", "black", "white" };
+
+        // Random.Shared.GetItems() creates an array populated with items chosen at random from the provided set of choices.
+        var colorsSelectedRandomly = Random.Shared.GetItems<string>(choices: colors, length: 3);
+
+        // magenta, magenta, blue
+        // N.B. Can be repeated!
+        foreach (var item in colorsSelectedRandomly) { Console.WriteLine(item); }
+
+        Console.WriteLine(new string('-', 10));
+
+        // --------------- Shuffle<T>() ---------------
+
+        var animals = new[] { "ant", "bee", "cat", "dog", "elephant", "fox", "goat" };
+
+        // Random.Shared.Shuffle() performs an in-place shuffle of an array.
+        Random.Shared.Shuffle<string>(animals);
+
+        foreach (var animal in animals) { Console.WriteLine(animal); }
+
+        Console.WriteLine(new string('-', 10));
+
+        // ==================== Data validation ====================
+        var validDataValidationObject = new DataValidationObject
+        {
+            RangedInt = 10,
+            MinMaxLengthString = "1234567890",
+            ValidBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes("This is a valid string that will be converted to Base64.")),
+            AllowedValuesString = "apple",
+            DeniedValuesString = "car"
+        };
+
+        ValidateObject(validDataValidationObject, nameof(validDataValidationObject));
+
+        var invalidDataValidationObject = new DataValidationObject
+        {
+            RangedInt = 9,
+            MinMaxLengthString = "123456789",
+            ValidBase64 = "invalidVGhpcyBpcyBhIHZhbGlkIHN0cmluZyB0aGF0IHdpbGwgYmUgY29udmVydGVkIHRvIEJhc2U2NC4=",
+            AllowedValuesString = "car",
+            DeniedValuesString = "apple"
+        };
+
+        ValidateObject(invalidDataValidationObject, nameof(invalidDataValidationObject));
+
+        static void ValidateObject(object objectToValidate, string nameOfObject)
+        {
+            var validationResult = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(objectToValidate, new ValidationContext(objectToValidate), validationResult, true);
+
+            Console.WriteLine($"The validation of object '{nameOfObject}' returned '{result}'.");
+
+            if (!result && validationResult != null && validationResult.Count != 0)
+            {
+                foreach (var item in validationResult) { Console.WriteLine(item.ErrorMessage); }
+            }
+        }
+    }
+}
